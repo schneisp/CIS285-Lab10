df089905453f552478bfc78321307294c8c4e1f1
schneisp <71292451+schneisp@users.noreply.github.com>
2024-04-11 20:41:07 -0400

    worked on testMixed and testDuplicates

--- src/basicSelectionSort/testSelectionSort.java
+++ src/basicSelectionSort/testSelectionSort.java
@@ -44,12 +44,41 @@ public class testSelectionSort {
 
 	}
 
+	@Test
 	public void testMixed(){
-
+		mySort = new SelectionSort();
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = -9;
+		arr[2] = 0;
+		arr[3] = -10;
+		arr[4] = 2;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -10;
+		Sortedarr[1] = -9;
+		Sortedarr[2] = 0;
+		Sortedarr[3] = 2;
+		Sortedarr[4] = 8;
+		System.out.println(Arrays.toString(mySort.basicSelectionSort(arr)));
+		assertArrayEquals(Sortedarr, mySort.basicSelectionSort(arr));
 	}
-
+	@Test
 	public void testDuplicates(){
-
+		mySort = new SelectionSort();
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = -9;
+		arr[2] = 7;
+		arr[3] = 8;
+		arr[4] = 2;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -9;
+		Sortedarr[1] = 2;
+		Sortedarr[2] = 7;
+		Sortedarr[3] = 8;
+		Sortedarr[4] = 8;
+		System.out.println(Arrays.toString(mySort.basicSelectionSort(arr)));
+		assertArrayEquals(Sortedarr, mySort.basicSelectionSort(arr));
 	}
 
 }
